30fd0ebc1f868bd7283ca1367b3dc59608a5555b
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2022-03-04 20:30:38 -0500

    fix sona repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -126,6 +126,9 @@ release {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
     }
 }
