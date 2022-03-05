41ec296c249d529f6070b71a1bb91e57c7d00cc1
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2022-03-05 18:35:21 -0500

    correctly handle dependencies

--- src/main/kotlin/com/sugarmanz/npm/Dependencies.kt
+++ src/main/kotlin/com/sugarmanz/npm/Dependencies.kt
@@ -1,8 +1,13 @@
 package com.sugarmanz.npm
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
-@Serializable
+@Serializable(Dependencies.Serializer::class)
 @JvmInline
 value class Dependencies(
     val dependencies: List<Dependency>
@@ -12,6 +17,23 @@ value class Dependencies(
 
     companion object {
         val Empty = Dependencies(emptyList())
+    }
+
+    object Serializer : KSerializer<Dependencies> {
+
+        private val structureSerializer = MapSerializer(Name.serializer(), Semver.serializer())
+
+        override val descriptor: SerialDescriptor = structureSerializer.descriptor
+
+        override fun deserialize(decoder: Decoder): Dependencies = decoder
+            .decodeSerializableValue(structureSerializer)
+            .entries
+            .map { (name, version) -> Dependency(name, version) }
+            .let(::Dependencies)
+
+        override fun serialize(encoder: Encoder, value: Dependencies) = value.dependencies
+            .associate { it.name to it.version }
+            .let { encoder.encodeSerializableValue(structureSerializer, it) }
     }
 }
 
